e506c6188c9a6f1d5554001ac44c3d795f3867e8
Юрий <ura280@mail.ru>
2026-04-13 15:29:29 +0300

    addL nginx changes

--- backend/src/app.py
+++ backend/src/app.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://178.72.186.112",
     ],
     allow_credentials=True,
     allow_methods=["*"],
